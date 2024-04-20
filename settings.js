115c25e64ce60e34da88603701f92c319ef20045
M-cross1 <154022207+M-cross1@users.noreply.github.com>
2024-04-21 00:35:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -72,7 +72,7 @@ global.author = process.env.AUTHER_NAME || "MADE BY KING M-CROSS"
 //________________________________________________________________________________________________________
 global.creator = process.env.GL_DEVS || "2349042566473@s.whatsapp.net"
 //________________________________________________________________________________________________________
-global.xprefix = process.env.PREFIX || '!'
+global.xprefix = process.env.PREFIX || '✔️'
 //________________________________________________________________________________________________________
 global.premium = process.env.GL_VIP || ["2349042566473"] // Premium User
 //________________________________________________________________________________________________________
@@ -81,7 +81,7 @@ global.hituet = 0
 
 //bot sett
 //________________________________________________________________________________________________________
-global.typemenu = process.env.MENU_DESIGN || 'v4' // menu type 'v1' => 'v8'
+global.typemenu = process.env.MENU_DESIGN || 'v2' // menu type 'v1' => 'v8'
 //________________________________________________________________________________________________________
 global.typereply = process.env.REPLY_TYPE || 'v2' // reply type 'v1' => 'v3'
 //________________________________________________________________________________________________________
@@ -89,7 +89,7 @@ global.autoblocknumber = process.env.AUTO_BAN || '212' //set autoblock country c
 //________________________________________________________________________________________________________
 global.antiforeignnumber = process.env.AUTO_BLOCK || '212' //set anti foreign number country code
 //________________________________________________________________________________________________________
-global.welcome = process.env.WELCOME_MSG || false //welcome/left in groups
+global.welcome = process.env.WELCOME_MSG || null //welcome/left in groups
 //________________________________________________________________________________________________________
 global.anticall = process.env.GL_NOCALL || false //bot blocks user when called
 //________________________________________________________________________________________________________
